9b64c40024e30ffb66202e1d4ec2cb8783e67d23
Joana Oliveira <97909025+JoanaLOliveira@users.noreply.github.com>
2025-11-25 11:23:02 -0300

    feat:  add classe Despesa

---
+++ scr/Despesa.java
@@ -0,0 +1,92 @@
+
+import java.util.Date;
+
+public class Despesa {
+    private String idDespesa;
+    private String nomeDespesa;
+    private double valor;
+    private Date data;
+    private Categoria categoria;
+
+    public Despesa(String idDespesa, String nomeDespesa, double valor, Date data, Categoria categoria) {
+        this.idDespesa = idDespesa;
+        this.nomeDespesa = nomeDespesa;
+        this.valor = valor;
+        this.data = data;
+        this.categoria = categoria;
+    }
+
+    public Despesa () {
+        
+    }
+
+    public String getIdDespesa() {
+        return idDespesa;
+    }
+
+    public void setIdDespesa(String idDespesa) {
+        this.idDespesa = idDespesa;
+    }
+
+    public String getNomeDespesa() {
+        return nomeDespesa; 
+    }
+
+    public void setNomeDespesa(String nomeDespesa) {
+        this.nomeDespesa = nomeDespesa;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+    
+    public Date getData() {
+        return data;
+    }
+    
+    public void setData(Date data) {
+        this.data = data;
+    }
+    
+    public Categoria getCategoria() {
+        return categoria;
+    }
+    
+    public void setCategoria(Categoria categoria) {
+        this.categoria = categoria;
+    }
+
+    @Override
+    public String toString() {
+        return "Despesa{" +
+                "idDespesa='" + idDespesa + '\'' +
+                ", nomeDespesa='" + nomeDespesa + '\'' +
+                ", valor=" + valor +
+                ", data=" + data +
+                ", categoria=" + categoria +
+                '}';
+    }
+
+    public void visualizarDespesa(){
+        System.out.println("ID da Despesa: " +idDespesa);
+        System.out.println("Nome da Despesa: " +nomeDespesa);
+        System.out.println("Valor da Despesa: " +valor);
+        System.out.println("Data da Despesa: " +data);
+        System.out.println("Categoria da Despesa: " +categoria.getNomeCategoria());
+    }
+
+    public static Despesa cadastrarDespesa(String nome, double valor, Date data, Categoria categoria) {
+        Despesa novaDespesa = new Despesa();
+        novaDespesa.setNomeDespesa(nome);
+        novaDespesa.setValor(valor);
+        novaDespesa.setData(data);
+        novaDespesa.setCategoria(categoria);
+        return novaDespesa;
+    }
+
+
+}
